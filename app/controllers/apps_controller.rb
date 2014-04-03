8e89eb6589e3873feb78a14ae78e17e063ed214c
Cevaris <cevaris@gmail.com>
2014-04-02 21:48:52 -0600

    started working on the main events dashboard

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -2,6 +2,10 @@ class AppsController < InheritedResources::Base
   respond_to :html, :json
   actions :all, except: [:show]
 
+  def events
+    @total_docs, @event_aggs = Event.name_per_hour(params)
+  end
+
   def permitted_params
     params.permit(app: [:name])
   end
